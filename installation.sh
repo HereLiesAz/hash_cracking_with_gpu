3a0801d31ed8f9487d9cfa49e75104ae5e6aa1a2
Az <103241502+HereLiesAz@users.noreply.github.com>
2025-08-21 18:49:24 -0500

    Update installation.sh

--- installation.sh
+++ installation.sh
@@ -3,11 +3,12 @@
 
 cd ../content/gdrive/My\ Drive/
 wget https://hashcat.net/files/hashcat-5.1.0.7z
-sudo apt install p7zip
+sudo apt-get install p7zip -y
 sudo p7zip -d hashcat-5.1.0.7z
 cd hashcat-5.1.0
 sudo cp hashcat64.bin /usr/bin/
 sudo ln -s /usr/bin/hashcat64.bin /usr/bin/hashcat	
 sudo cp -Rv OpenCL/ /usr/bin/
 sudo cp hashcat.hcstat2 /usr/bin/
+
 sudo cp hashcat.hctune /usr/bin/
